f156f137095fd580021add931c162c551826ee87
Harshit Saxena <harshit214saxena@gmail.com>
2025-07-04 04:04:26 +0530

    Add HeroSection and Spotlight components with animations; update layout and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+ 
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ><div className="relative w-full flex items-center justify-center">
+      >
+         
+         <div className="relative w-full flex items-center justify-center">
+        
         <Navbar />
         </div>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { main } from "motion/react-client";
+import HeroSection from "@/components/HeroSection";
+import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to My Music App</h1>
-      <p className="mt-4 text-lg">Explore our courses and learn music theory, composition, songwriting, and production.</p>
+        <Spotlight
+        className="-top-40 left-0 md:-top-20 md:left-60"
+        fill="blue"
+      />
+       < HeroSection />
     </main>
 
   );
