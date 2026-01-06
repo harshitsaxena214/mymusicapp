873f599145a1945f4299728fc428b5ece9ebc5df
Harshit Saxena <harshit214saxena@gmail.com>
2026-01-07 01:21:06 +0530

    feat: Add music1.svg and WhyChooseUs component; implement StickyScroll for enhanced user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,18 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
- 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "My Music App",
   description: "Discover original beats, soulful melodies, and the rhythm that moves you.", 
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
